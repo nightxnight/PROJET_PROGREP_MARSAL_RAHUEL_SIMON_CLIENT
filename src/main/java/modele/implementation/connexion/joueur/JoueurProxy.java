d5397a5d582502046574d2c28b690b9a7c5d8bf6
Rémi <remi.marsal57@gmail.com>
2021-03-25 19:24:15 +0100

    Ajout de scroll pane

---
+++ src/main/java/modele/implementation/connexion/joueur/JoueurProxy.java
@@ -0,0 +1,21 @@
+package modele.implementation.connexion.joueur;
+
+import java.io.Serializable;
+
+public class JoueurProxy implements Serializable {
+
+    private final String pseudo;
+    private boolean enLigne;
+
+    public JoueurProxy(String pseudo, boolean enLigne) {
+        this.pseudo = pseudo;
+        this.enLigne = enLigne;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+    public boolean isEnLigne() {
+        return enLigne;
+    }
+}
